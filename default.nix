5f264e0c130815d5a59c56367442fd5f26ba94ba
Isaac Elliott <isaace71295@gmail.com>
2018-11-23 09:08:53 +1000

    Revert

--- default.nix
+++ default.nix
@@ -1,11 +1,5 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
-nixpkgs.stdenv.mkDerivation {
-  name = "variant1";
-  src = ./.;
-  buildInputs =
-    [ (import ./variant1 { inherit nixpkgs compiler; })
-      (import ./variant1-lens { inherit nixpkgs compiler; })
-    ];
-  installPhase = ''ls'';
-  outputs = [ "variant1" "variant1-lens" ];
+{
+  variant1 = import ./variant1 { inherit nixpkgs compiler; };
+  variant1-lens = mport ./variant1-lens { inherit nixpkgs compiler; };
 }
