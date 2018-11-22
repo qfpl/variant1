9b80818ff76de84628ee5a85ad8f3f158db9658f
Isaac Elliott <isaace71295@gmail.com>
2018-11-23 09:09:44 +1000

    Fixed typo

--- default.nix
+++ default.nix
@@ -1,5 +1,5 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
 {
   variant1 = import ./variant1 { inherit nixpkgs compiler; };
-  variant1-lens = mport ./variant1-lens { inherit nixpkgs compiler; };
+  variant1-lens = import ./variant1-lens { inherit nixpkgs compiler; };
 }
