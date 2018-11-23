d6be9f76891d3e78d45643c376f8245641ac770a
Isaac Elliott <isaace71295@gmail.com>
2018-11-23 10:47:36 +1000

    Revert

--- default.nix
+++ default.nix
@@ -1,4 +1,5 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
 {
-  variant1 = ./variant1;
-  variant1-lens = ./variant1-lens;
+  variant1 = import ./variant1 { inherit nixpkgs compiler; };
+  variant1-lens = import ./variant1-lens { inherit nixpkgs compiler; }
 }
